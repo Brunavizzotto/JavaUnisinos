6b0c2f964d4763ec4979e3f475319ea5a3781686
Brunavizzotto <bruna.vizzotto0@gmail.com>
2026-04-04 16:25:15 -0300

    primeira tentativa de rodar java no terminal

---
+++ src/main/java/app/Banana.java
@@ -0,0 +1,17 @@
+package app;
+
+public class Banana {
+    public static void main(String[] args) {
+        int banana1 = 12;
+        short banana2 = 10;
+        float banana3 = 8.5f;
+        double banana4 = 48.7;
+        long banana5 = 123456789L;
+
+        System.out.println("O valor da banana1: " + banana1);
+        System.out.println("O valor da banana2: " + banana2);
+        System.out.println("O valor da banana3: " + banana3);
+        System.out.println("O valor da banana4: " + banana4);
+        System.out.println("O valor da banana5: " + banana5);
+    }
+}

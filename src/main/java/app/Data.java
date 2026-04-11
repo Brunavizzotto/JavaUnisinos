219672b0145aa8106ecbc47d68b9334cc148c29e
Brunavizzotto <bruna.vizzotto0@gmail.com>
2026-04-11 13:58:20 -0300

    nome da classe deve ter o mesmo nome do arquivo

---
+++ src/main/java/app/Data.java
@@ -0,0 +1,9 @@
+package app;
+
+public class Data {
+
+    public static void main(String[] args) {
+        System.out.printf("Nome: %s | Idade: %d%n", nome, idade);
+        System.out.println();("(to java programming)", "welcome to java programming");
+    }
+}

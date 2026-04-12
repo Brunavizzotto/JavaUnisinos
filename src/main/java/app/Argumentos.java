576718d5b0b53bb8980a7278a9422f1f38a7fa49
Brunavizzotto <bruna.vizzotto0@gmail.com>
2026-04-12 18:14:14 -0300

    String + argumentos

---
+++ src/main/java/app/Argumentos.java
@@ -0,0 +1,7 @@
+package app;
+
+class Argumentos {
+    public static void main (String[] args){
+        System.out.println("voce digitou " + args[0]);
+    }    
+}
---
+++ src/main/java/app/Argumentos2.java
@@ -0,0 +1,7 @@
+package app;
+
+public class Argumentos2 {
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo " + args[0]);
+    }
+}

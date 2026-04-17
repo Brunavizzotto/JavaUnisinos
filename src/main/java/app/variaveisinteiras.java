535f9c3383d1bcc23c1dd7c8dda09088544e2afe
Brunavizzotto <bruna.vizzotto0@gmail.com>
2026-04-16 21:22:58 -0300

    ASCII das variaveis 'i' e 'o'

---
+++ src/main/java/app/variaveisinteiras.java
@@ -0,0 +1,15 @@
+package app;
+
+public class variaveisinteiras {
+    public static void main(String[] args) {
+        double passagem = 5.59;
+
+        char o = 'o';
+        char i = 'i';
+
+        System.out.println(o+i);
+        System.out.println("O valor da passagem é " + passagem);
+        
+    }
+    
+}

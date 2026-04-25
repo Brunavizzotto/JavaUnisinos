ecd3506d667b12baeef33b220e1bf1f8887f757c
Brunavizzotto <bruna.vizzotto0@gmail.com>
2026-04-24 21:14:33 -0300

    shopping (trabalho unisinos)

---
+++ src/main/java/app/Loja.java
@@ -0,0 +1,12 @@
+public class Loja {
+    private String nome;
+    private int quantidadeFuncionarios;
+    private double salarioBaseFuncionario;
+
+    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+}
